9ada5427cb1471b16e6ea1d149c2416a6d2229d4
jinjiazhang <jinjiazh@qq.com>
2017-12-27 10:34:15 +0800

    lua code format

--- protoxls/LuaExporter.cpp
+++ protoxls/LuaExporter.cpp
@@ -2,6 +2,26 @@
 #include "LuaExporter.h"
 #include "ParseHelper.h"
 
+#define FLUSH_SPACE(code) do {(code) << " ";} while(0)
+#define FLUSH_NEWLINE(code) do {(code) << "\n";} while(0)
+#define FLUSH_LAYER(code, layer) do {for (int i=0; i<(layer); i++) {(code) << "\t";}} while(0)
+
+#define OUTER_SPACE(code)           FLUSH_SPACE(code)
+#define OUTER_NEWLINE(code)         FLUSH_NEWLINE(code)
+#define OUTER_LAYER(code, layer)    FLUSH_LAYER(code, layer)
+
+#define INNER_SPACE(code)           // FLUSH_SPACE(code)
+#define INNER_NEWLINE(code)         // FLUSH_NEWLINE(code)
+#define INNER_LAYER(code, layer)    // FLUSH_LAYER(code, layer)
+
+#define TABLE_SPACE(code)           // FLUSH_SPACE(code)
+#define TABLE_NEWLINE(code)         // FLUSH_NEWLINE(code)
+#define TABLE_LAYER(code, layer)    // FLUSH_LAYER(code, layer)
+
+#define ARRAY_SPACE(code, field)            // {if ((field)->message_type()) FLUSH_SPACE(code);}
+#define ARRAY_NEWLINE(code, field)          // {if ((field)->message_type()) FLUSH_NEWLINE(code);}
+#define ARRAY_LAYER(code, field, layer)     // {if ((field)->message_type()) FLUSH_LAYER(code, layer);}
+
 bool LuaExporter::ExportResult(ConfigStore* store)
 {
     const Descriptor* descriptor = store->GetDescriptor();
@@ -28,12 +48,11 @@ bool LuaExporter::ExportResult(ConfigStore* store)
     return true;
 }
 
-#define FLUSH_LAYER(code, layer) do {for (int i = 0; i < (layer); i++) { code << "\t";}} while(0)
-
 string LuaExporter::GenerateCode(ConfigStore* store, int layer)
 {
     std::stringstream code;
-    code << "{\n";
+    code << "{";
+    OUTER_NEWLINE(code);
     
     vector<StoreKey> store_keys;
     store->ExportKeys(store_keys);
@@ -41,28 +60,30 @@ string LuaExporter::GenerateCode(ConfigStore* store, int layer)
 
     for (int i = 0; i < store_keys.size(); i++)
     {
-        FLUSH_LAYER(code, layer + 1);
+        OUTER_LAYER(code, layer + 1);
         StoreKey store_key = store_keys[i];
         if (store_key.key_type == KEY_TINTEGER)
             code << "[" << store_key.num_key << "]";
         else
             code << "['" << store_key.str_key << "']"; 
 
-        code << " = ";
+        OUTER_SPACE(code);
+        code << "=";
+        OUTER_SPACE(code);
+
         ConfigStore* sub_store = store->GetConfig(store_key);
         if (sub_store->HasStoreMap())
-        {
             code << GenerateCode(sub_store, layer + 1);
-        }
         else
-        {
-            code << GenerateCode(*sub_store->GetData());
-        }
+            code << GenerateCode(*sub_store->GetData(), layer + 1);
 
-        code << ",\n";
+        if (i < store_keys.size() - 1) {
+            code << ",";
+        }
+        OUTER_NEWLINE(code);
     }
 
-    FLUSH_LAYER(code, layer);
+    OUTER_LAYER(code, layer);
     code << "}";
     return code.str();
 }
@@ -71,19 +92,25 @@ string LuaExporter::GenerateCode(const Message& message, int layer)
 {
     std::stringstream code;
     code << "{";
+    INNER_NEWLINE(code);
     
     const Descriptor* descriptor = message.GetDescriptor();
     for (int i = 0; i < descriptor->field_count(); i++)
     {
+        INNER_LAYER(code, layer + 1);
         const FieldDescriptor* field = descriptor->field(i);
         code << field->name();
+        INNER_SPACE(code);
         code << "=";
+        INNER_SPACE(code);
         code << GenerateField(message, field, layer + 1);
         if (i < descriptor->field_count() - 1) {
             code << ", ";
         }
+        INNER_NEWLINE(code);
     }
 
+    INNER_LAYER(code, layer);
     code << "}";
     return code.str();
 }
@@ -106,6 +133,7 @@ string LuaExporter::GenerateTable(const Message& message, const FieldDescriptor*
 {
     std::stringstream code;
     code << "{";
+    TABLE_NEWLINE(code);
 
     const Reflection* reflection = message.GetReflection();
     int field_size = reflection->FieldSize(message, field);
@@ -116,6 +144,7 @@ string LuaExporter::GenerateTable(const Message& message, const FieldDescriptor*
 
     for (int index = 0; index < field_size; index++)
     {
+        TABLE_LAYER(code, layer + 1);
         const Message& submessage = reflection->GetRepeatedMessage(message, field, index);
         StoreKey store_key;
         ConfigStore::GetKeyVal(submessage, key->name(), &store_key);
@@ -124,14 +153,18 @@ string LuaExporter::GenerateTable(const Message& message, const FieldDescriptor*
         else
             code << "['" << store_key.str_key << "']";
 
-        code << " = ";
+        TABLE_SPACE(code);
+        code << "=";
+        TABLE_SPACE(code);
         code << GenerateField(submessage, value, layer);
 
         if (index < field_size - 1) {
             code << ", ";
         }
+        TABLE_NEWLINE(code);
     }
 
+    TABLE_LAYER(code, layer);
     code << "}";
     return code.str();
 }
@@ -183,12 +216,14 @@ string LuaExporter::GenerateMultiple(const Message& message, const FieldDescript
 {
     std::stringstream code;
     code << "{";
+    ARRAY_NEWLINE(code, field);
 
     const Reflection* reflection = message.GetReflection();
     int field_size = reflection->FieldSize(message, field);
 
     for (int index = 0; index < field_size; index++)
     {
+        ARRAY_LAYER(code, field, layer + 1);
         const Reflection* reflection = message.GetReflection();
         switch (field->cpp_type())
         {
@@ -230,8 +265,10 @@ string LuaExporter::GenerateMultiple(const Message& message, const FieldDescript
         if (index < field_size - 1) {
             code << ", ";
         }
+        ARRAY_NEWLINE(code, field);
     }
 
+    ARRAY_LAYER(code, field, layer);
     code << "}";
     return code.str();
 }
